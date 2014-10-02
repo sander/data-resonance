3c4bdc892820d328459a6733ed86cd373296e28c
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-10-02 13:42:11 +0200

    Changes

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                           [org.clojure/data.priority-map "0.0.5"]
                            [quil "2.2.2"]
                            [overtone "0.9.1"]]
             :main ^:skip-aot wifi.core
--- src/wifi/core.clj
+++ src/wifi/core.clj
@@ -1,13 +1,14 @@
 (ns wifi.core
   (:require [quil.core :as q]
-            ;[overtone.core :as overtone]
+            [overtone.core :as overtone]
             [clojure.core.async :as async]
-            [clojure.core.async.lab :as async-lab])
+            [clojure.core.async.lab :as async-lab]
+            [clojure.data.priority-map :refer [priority-map]])
   (:gen-class))
 
-;(defonce sc-server (overtone/boot-server))
-;(def pop-sample (overtone/sample "/System/Library/Sounds/Pop.aiff"))
-;(def tink-sample (overtone/sample "/System/Library/Sounds/Tink.aiff"))
+(defonce sc-server (overtone/boot-server))
+(def pop-sample (overtone/sample "/System/Library/Sounds/Pop.aiff"))
+(def tink-sample (overtone/sample "/System/Library/Sounds/Tink.aiff"))
 
 (def fields {:frame.number read-string
               ;:ip.addr
@@ -65,6 +66,8 @@
 (def counter (atom 0))
 
 (def ch (async-lab/spool only-data))
+(def lastp (atom nil))
+
 ;(async/close! ch)
 (async/go-loop []
                (let [p (async/<! ch)]
@@ -76,7 +79,6 @@
                      (recur)))))
 
 (def total (atom 0))
-(def lastp (atom nil))
 (def ch2 (async-lab/spool proc))
 (async/go-loop []
                (let [p (async/<! ch2)]
@@ -84,6 +86,70 @@
                    (do
                      (swap! total inc)
                      (recur)))))
+
+(def addresses (atom (priority-map)))
+(def ch3 (async-lab/spool only-data))
+(defn update-address [addr]
+  (if-not (contains? @addresses addr)
+    (swap! addresses assoc addr 0))
+  (swap! addresses update-in [addr] inc))
+(async/go-loop []
+               (let [p (async/<! ch3)]
+                 (update-address (:wlan.sa p))
+                 (update-address (:wlan.da p))
+                 (recur)))
+(println (take 5 (rseq @addresses)))
+
+;; counter
+(def interval 1000)
+(def ival-count (atom 0))
+(def delay (atom 1000))
+(def delay-based-on (atom 0))
+(def factor 50)
+(def ch5 (async-lab/spool only-data))
+(async/go-loop []
+               (let [p (async/<! ch5)]
+                 (swap! ival-count inc)
+                 (recur)))
+(async/go-loop []
+               (async/<! (async/timeout interval))
+               (reset! delay (if (> @ival-count 0)
+                               (int (* factor (/ interval (double @ival-count))))
+                               interval))
+               (reset! delay-based-on @ival-count)
+               (reset! ival-count 0)
+               (recur))
+
+;; own sounds
+(def skip 50)
+(def own-count (atom 0))
+(def ch6 (async-lab/spool only-data))
+(def self "24:e3:14:26:72:4e")
+(def self "28:94:0f:aa:97:5f")
+(defn own-loop []
+  (let [p (async/<!! ch6)]
+    (if (or (= (:wlan.sa p) self) (= (:wlan.da p) self))
+      (do
+        (swap! own-count inc)
+        (if (> @own-count skip)
+          (do
+            (tink-sample)
+            (reset! own-count 0)))))))
+(async/go-loop []
+               (own-loop)
+               (recur))
+
+;;delay is too big
+
+;; play sounds
+(async/go-loop []
+               (if (= @delay-based-on 0)
+                 nil
+                 (pop-sample))
+               (async/<! (async/timeout @delay))
+               (recur))
+
+;; may be interesting to differentiate signals sent by the most present MAC address (probably the router)
 
 (defn sketch []
   (defn setup []
@@ -95,8 +161,18 @@
     ;(q/stroke-weight (q/random 10))
     ;(q/fill 0 0 (q/random 255))
     (q/background 0)
-    (q/text (str @counter "/" @total) 0 100)
-    (q/text (str @lastp) 0 130)
+    (q/text (str @counter "/" @total " (" @own-count ")") 0 30)
+    (q/text (str @lastp) 0 60)
+    (q/text (reduce
+              str
+              (map
+                (fn [[addr count]]
+                  (str addr ": " count "\n"))
+                (take 5 (rseq @addresses))))
+            0 90)
+
+    (q/fill 255)
+    (q/ellipse (/ (q/width) 2) (/ (q/height) 2) 50)
 
     (let [diam (q/random 100)
           ;      x (q/random (q/width))
