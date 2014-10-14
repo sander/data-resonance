ec19e056bc3a227976e31838384d997b24f312eb
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-10-14 12:12:36 +0200

    Make wifi.haptic a bit cleaner

--- src/wifi/haptic.clj
+++ src/wifi/haptic.clj
@@ -1,46 +1,97 @@
 (ns wifi.haptic
   (:require [serial-port :as serial]
-            [quil.core :as q]
+            ;[quil.core :as q]
             [clojure.core.async :as async :refer [chan alts! go go-loop timeout <! >! put!]]))
 
-(def arduino (serial/open "/dev/tty.usbmodem1421"))
+(comment
+  ;; path => serial-port
+  (def connections (atom {}))
+
+  (defn connect
+    ([path]
+     (if-let [conn (get @connections path)]
+       (serial/close conn))
+     (let [port (serial/open path)]
+       (swap! connections assoc path port)
+       port))
+    ([] (connect "/dev/tty.usbmodem1421"))))
+
+(defn open
+  ([path] (serial/open path))
+  ([] (open "/dev/tty.usbmodem1421")))
+(defonce port (atom (open)))
+(defn close
+  []
+  (serial/close @port))
+(defn reopen
+  []
+  (swap! port open))
+
 (def last-motor-status (atom [0 0]))
 
 (def fps 60)
 
-;;(serial/on-byte arduino #(println "byte: " (char %)))
-(defn set-motors [m1 m2]
+(defn set-motors
+  [m1 m2]
   (reset! last-motor-status [m1 m2])
-  (serial/write arduino (byte-array [255 m1 m2])))
-(defn manual []
+  (serial/write @port (byte-array [255 m1 m2])))
+(defn manual
+  []
   (reset! last-motor-status [0 0])
-  (serial/write arduino (byte-array [254])))
-(defn close []
-  (serial/close arduino))
+  (serial/write @port (byte-array [254])))
 ;;(close)
 
-(defn listen []
+(defn reset
+  []
+  ;; todo
+  (set-motors 40 30))
+(defn sink-slowly
+  []
+  ;; todo
+  (set-motors 50 60)
+  )
+(defn raise-slowly []
+  ;; todo
+  )
+
+(defn listen
+  []
   (let [ch (chan)
         status (atom 0)]
-    (serial/on-byte arduino (fn [st]
-                              (when (not= @status st)
-                                (case st
-                                  1 (put! ch :release)
-                                  2 (put! ch :press)))
-                              (reset! status st)))
+    (serial/on-byte @port (fn [st]
+                            (when (not= @status st)
+                              (case st
+                                1 (put! ch :release)
+                                2 (put! ch :press)
+                                nil))
+                            (reset! status st)))
     ch))
-(defn unlisten []
-  (serial/remove-listener arduino))
-(defn ix [touch stop]
-  (go-loop []
-           (let [[msg c] (alts! [touch stop])]
-             (condp = [msg c]
-               [:release touch] (reset)
-               [:press touch] (sink-slowly)
-               nil)
-             (if (not= c stop) (recur)))))
+(defn unlisten
+  []
+  (serial/remove-listener @port))
+(defonce ix-instance (atom nil))
+(defn ix
+  ([]
+   (if @ix-instance
+     (@ix-instance))
+   (let [li (listen)
+         st (chan)]
+     (ix li st)
+     (reset! ix-instance
+             (fn []
+               (put! st true)
+               (unlisten)))))
+  ([touch stop]
+   (go-loop []
+            (let [[msg c] (alts! [touch stop])]
+              (condp = [msg c]
+                [:release touch] (reset)
+                [:press touch] (sink-slowly)
+                nil)
+              (if (not= c stop) (recur))))))
 
-(defmacro dotime [expr]
+(defmacro dotime
+  [expr]
   `(let [start# (System/nanoTime)]
      ~expr
      (/ (double (- (System/nanoTime) start#)) 1e6)))
@@ -69,32 +120,22 @@
          (set-motors v10 v20)
          (reset! vibrating false))))))
 
-(defn reset []
-  ;; todo
-  (set-motors 40 30))
-(defn sink-slowly []
-  ;; todo
-  (set-motors 50 60)
-  )
-(defn raise-slowly []
-  ;; todo
-  )
-
-;(reset)
+(reset)
 (sink-slowly)
 
-(defn sketch []
-  (defn setup []
-    (q/frame-rate 30))
-  (defn draw []
-    (q/background 255)
-    (q/fill 0)
-    (let [m1 (q/round (q/map-range (q/mouse-x) 0 (q/width) 0 180))
-          m2 (q/round (q/map-range (q/mouse-y) 0 (q/height) 0 180))]
-      (q/text (str m1 ":" m2) 50 50)
-      (set-motors m1 m2)))
-  (q/defsketch testsketch
-               :title "motor test"
-               :setup setup
-               :draw draw
-               :size [600 400]))
+(comment
+  (defn sketch []
+   (defn setup []
+     (q/frame-rate 30))
+   (defn draw []
+     (q/background 255)
+     (q/fill 0)
+     (let [m1 (q/round (q/map-range (q/mouse-x) 0 (q/width) 0 180))
+           m2 (q/round (q/map-range (q/mouse-y) 0 (q/height) 0 180))]
+       (q/text (str m1 ":" m2) 50 50)
+       (set-motors m1 m2)))
+   (q/defsketch testsketch
+                :title "motor test"
+                :setup setup
+                :draw draw
+                :size [600 400])))
